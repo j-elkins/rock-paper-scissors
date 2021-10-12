e9d593416c4584ccb2a9790ac14f10b8890e398f
j-elkins <82281911+j-elkins@users.noreply.github.com>
2021-10-12 11:20:45 -0400

    create index.js, write pseudo code

---
+++ index.js
@@ -0,0 +1,15 @@
+// write function computerPlay that randomly returns 1 of 3 values
+
+// write  function singleRound that plays one round
+// takes two parameters: playerSelection and computerSelection
+// playerSelection must be case insensitive
+
+// when the function runs it returns a string to declare the winner
+// format "You Lose! x beats y"
+
+// return results of this function
+
+// write function game() to run singleRound 5 times (either loop or run 5 times)
+// console.log results of each round
+// function game() keeps score and reports winner or loser at the end
+// prompt() gets input from user
