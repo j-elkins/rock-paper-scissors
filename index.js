d8bf2b5b3c0760533221aa93b274a1302a41fb4f
Jasmine Elkins <jasmine.elkins@gmail.com>
2021-10-29 20:42:10 -0400

    Added buttons and result/counter

--- index.js
+++ index.js
@@ -18,58 +18,101 @@ function askUser() {
   }
   return answer;
 }
-// write  function singleRound that plays one round
+
+// write  function playSingleRound that plays one round
 // takes two parameters: playerSelection and computerSelection
-function singleRound() {
-  let playerSelection = askUser();
+function playSingleRound(buttonClicked) {
+  let playerSelection = buttonClicked;
+  console.log("Player chooses: " + playerSelection);
   let computerSelection = computerPlay(optionsList);
   console.log("Computer chooses: " + computerSelection);
 
   // returns a string to declare winner, format "You Lose! x beats y"
   // function needs to know rules:  rock >  scissors,  scissors >  paper,  paper >  rock
 
+  // create a div to show results
+  // change console.logs into DOM methods
+  const game = document.querySelector("#game");
+  const resultsDiv = document.createElement("div");
+  resultsDiv.classList.add("results");
+
   if (playerSelection == computerSelection) {
-    console.log("It's a tie!");
+    resultsDiv.textContent = "It's a tie!";
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    console.log("Paper beats rock. You lose!");
+    resultsDiv.textContent = "Paper beats rock. You lose!";
     computerPoints += 1;
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    console.log("Rock beats scissors. You win!");
+    resultsDiv.textContent = "Rock beats scissors. You win!";
     playerPoints += 1;
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log("Paper beats rock. You win!");
+    resultsDiv.textContent = "Paper beats rock. You win!";
     playerPoints += 1;
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    console.log("Scissors beat paper. You lose!");
+    resultsDiv.textContent = "Scissors beat paper. You lose!";
     computerPoints += 1;
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    console.log("Rock beats scissors. You lose!");
+    resultsDiv.textContent = "Rock beats scissors. You lose!";
     computerPoints += 1;
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    console.log("Scissors beat paper. You win!");
+    resultsDiv.textContent = "Scissors beat paper. You win!";
     playerPoints += 1;
+  }
+
+  game.appendChild(resultsDiv);
+
+  const compResults = document.createElement("div");
+  compResults.textContent = "computer: " + computerPoints;
+  game.appendChild(compResults);
+
+  const playerResults = document.createElement("div");
+  playerResults.textContent = "player: " + playerPoints;
+  game.appendChild(playerResults);
+
+  if (playerPoints == 5) {
+    return "Congratulations! You win";
+  }
+
+  if (computerPoints == 5) {
+    return "Sorry, you lose. Play again?";
   }
 }
 
-// write function game() to run singleRound 5 times (either loop or run 5 times)
+// instead of prompt, use buttons to choose playerSelection
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+  let buttonClicked = "rock";
+  playSingleRound(buttonClicked);
+});
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+  let buttonClicked = "paper";
+  playSingleRound(buttonClicked);
+});
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+  let buttonClicked = "scissors";
+  playSingleRound(buttonClicked);
+});
+
+// write function game() to run playSingleRound 5 times (either loop or run 5 times)
 // console.log results of each round and reports winner or loser at the end
 const numberOfTurns = 5;
 let turnIndex = 0;
 
 function playGame() {
-  while (turnIndex < numberOfTurns) {
-    singleRound();
-    turnIndex++;
-    console.log(
-      "ROUND " +
-        turnIndex +
-        ". " +
-        "Player: " +
-        playerPoints +
-        " Computer: " +
-        computerPoints
-    );
-  }
+  // while (turnIndex < numberOfTurns) {
+  playSingleRound();
+  // turnIndex++;
+  // console.log(
+  //   "ROUND " +
+  //     turnIndex +
+  //     ". " +
+  //     "Player: " +
+  //     playerPoints +
+  //     " Computer: " +
+  //     computerPoints
+  // );
+  // }
   if (playerPoints > computerPoints) {
     console.log("Congratulations! You are the winner!");
   } else if (computerPoints > playerPoints) {
@@ -79,4 +122,4 @@ function playGame() {
   }
 }
 
-playGame();
+// playGame();
